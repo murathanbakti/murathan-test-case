60af37c9b7f9d1bd42ea7444c9b554f5b0ac98df
guross <murathanbakti@gmail.com>
2023-03-03 15:38:39 +0300

    style: user task page is done

--- src/pages/User/UserPage.js
+++ src/pages/User/UserPage.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
+import "./userPage.css";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const UserPage = () => {
   const [data, setData] = useState();
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+  const navigate = useNavigate()
   useEffect(() => {
     const handleTask = () => {
-      const cuurentUser = JSON.parse(localStorage.getItem("user"));
-      console.log(cuurentUser);
+      console.log(currentUser);
       try {
-        fetch(`http://localhost:3000/tasks?owner=${cuurentUser.username}`)
+        fetch(`http://localhost:3000/tasks?owner=${currentUser.username}`)
           .then((res) => {
             return res.json();
           })
@@ -23,17 +26,37 @@ const UserPage = () => {
     handleTask();
   }, []);
   console.log(data);
+  const addTask = () => {
+    navigate("/add-task")
+  };
   return (
     <div>
-      {!data ? <></> : <div className="container">
-        {data.map((e) => (
-          <div className="card">
-            <span className="cardTitle">{e.title}</span>
-            <span className="endTime">{e.time}</span>
-            <span className="cardDesc">{e.desc}</span>
-          </div>
-        ))}
-      </div>}
+      <div class="container">
+        <div className="todoList">
+          <h3 className="userName">Kullanıcı: {currentUser.username}</h3>
+          <h1>Todo List</h1>
+          {!data ? (
+            <></>
+          ) : (
+            <div className="container">
+              {data.map((e) => {
+                if (e.display === true) {
+                  return (
+                    <div className="card">
+                      <span className="cardTitle">{e.title}</span>
+                      <span className="endTime">{e.time}</span>
+                      <span className="cardDesc">{e.desc}</span>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          )}
+          <button className="addTask" onClick={addTask}>
+            +
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
